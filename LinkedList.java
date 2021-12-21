4e68ff953c2448587e4bdc49e9257ace2ee3a5c8
Srinath S <srinath@Srinaths-MacBook-Air.local>
2021-12-21 14:37:28 +0530

    First Commit

--- LinkedList.java
+++ LinkedList.java
@@ -5,13 +5,19 @@ public class LinkedList {
         LinkedListFunctions LLF = new LinkedListFunctions();
 
         LLF.insertFirst(56);
+        LLF.printNodeElement();
         LLF.insertLast(70);
+        LLF.printNodeElement();
         LLF.insertInBetween(56,30);
         LLF.printNodeElement();
         LLF.findNode(30);
         LLF.insertInBetween(30, 40);
         LLF.printNodeElement();
         LLF.sort();
+        LLF.printNodeElement();
+        LLF.deleteFirst();
+        LLF.printNodeElement();
+        LLF.deleteLast();
         LLF.printNodeElement();
     }
 }
